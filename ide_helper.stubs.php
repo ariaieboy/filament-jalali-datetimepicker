6ebb891fd853734619da20ab10548de6a28b25df
AriaieBOY <ariaieboy.ir@gmail.com>
2023-09-02 15:59:12 +0330

    add ide_helper

---
+++ ide_helper.stubs.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Filament\Forms\Components {
+
+    use Ariaieboy\FilamentJalaliDatetimepicker\FilamentJalaliDatetimepickerServiceProvider;
+
+    class DatePicker
+    {
+        /**
+         * @source FilamentJalaliDatetimepickerServiceProvider.php:21
+         */
+        public function jalali(): self
+        {
+            return $this;
+        }
+    }
+
+    class DateTimePicker
+    {
+        /**
+         * @source FilamentJalaliDatetimepickerServiceProvider.php:25
+         */
+        public function jalali(): self
+        {
+            return $this;
+        }
+    }
+}
